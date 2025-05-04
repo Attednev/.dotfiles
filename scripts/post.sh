5defe23a4c3c430d03fe836035a216fe3f39d07e
Damian Gasser <damian.gasser@gmail.com>
2025-05-04 17:48:48 +0200

    Add configuration scripts

---
+++ scripts/post.sh
@@ -0,0 +1,4 @@
+
+papirus-folders -C bluegrey --theme Papirus-Dark
+plasma-apply-cursortheme catppuccin-mocha-rosewater-cursors
+
---
+++ scripts/pre.sh
@@ -0,0 +1,11 @@
+
+sudo pacman -S git papirus-icon-theme nvim alacritty fish zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://aur.archlinux.org/yay.git $HOME/yay
+cd $HOME/yay
+makepkg -si --noconfirm
+
+yay -S catppuccin-cursors-mocha
+
