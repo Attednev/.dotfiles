611d93a583eb9a8af051330e797cc4133d495552
Damian Gasser <damian.gasser@gmail.com>
2025-06-03 15:49:28 +0200

    Add tmux-sessionizer installation

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-${HOME}/.local/state}"
-export XDG_CACHE_HOME="${XDG_CACHE_HOME:-${HOME}/.cache}"
+export XDG_BIN_HOME="${XDG_BIN_HOME:-${HOME}/.local/bin}"
 
 DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DOTFILES_DIR" || exit 1
--- scripts/pre.sh
+++ scripts/pre.sh
@@ -11,5 +11,8 @@ yay -S catppuccin-cursors-mocha
 mkdir -p "$XDG_DATA_HOME/tmux/plugins"
 mkdir -p "$XDG_STATE_HOME/tmux"
 
+curl -o "$XDG_BIN_HOME/tmux-sessionizer" "https://raw.githubusercontent.com/ThePrimeagen/tmux-sessionizer/refs/heads/master/tmux-sessionizer"
+chmod +x "$XDG_BIN_HOME/tmux-sessionizer"
+
 git clone https://github.com/tmux-plugins/tpm $XDG_DATA_HOME/tmux/plugins/tpm
 
