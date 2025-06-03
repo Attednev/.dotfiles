3a20e1a70d37612b1e887b865438ca42b4232bba
Damian Gasser <damian.gasser@gmail.com>
2025-06-03 15:34:01 +0200

    Update zsh config and plugins

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 papirus-folders -C bluegrey --theme Papirus-Dark
 plasma-apply-cursortheme catppuccin-mocha-rosewater-cursors
 
--- scripts/pre.sh
+++ scripts/pre.sh
@@ -2,8 +2,6 @@
 
 sudo pacman -S git papirus-icon-theme nvim alacritty fish zsh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 git clone https://aur.archlinux.org/yay.git $HOME/yay
 cd $HOME/yay
 makepkg -si --noconfirm
