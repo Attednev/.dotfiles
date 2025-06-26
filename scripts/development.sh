c4fd459033e75781b1104e32d584f72e26033b27
Damian Gasser <damian.gasser@gmail.com>
2025-06-26 17:34:17 +0200

    Add npm as a dependency

--- scripts/development.sh
+++ scripts/development.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --needed git clang make rustup base-devel
+sudo pacman -S --noconfirm --needed git clang make rustup base-devel npm
 
 mkdir -p "$HOME/.config/clang-format"
 curl -o "$HOME/.config/clang-format/custom.clang-format" "https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/plain/.clang-format"
